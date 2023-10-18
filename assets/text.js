725a0356b141b4792774e6be15a910313d8aea18
rischky <148265771+rischky@users.noreply.github.com>
2023-10-18 16:24:42 +0700

    Update text.js

--- assets/text.js
+++ assets/text.js
@@ -1,9 +1,17 @@
-// Ambil elemen dengan ID "toggleButton" dan elemen dengan kelas "text-box"
 const toggleButton = document.getElementById("toggleButton");
 const textBox = document.querySelector(".text-box");
 
 // Tambahkan event listener untuk menangani klik pada tombol
-toggleButton.addEventListener("click", function () {
+toggleButton.addEventListener("click", function (event) {
   // Toggle kelas "active" pada elemen "text-box"
   textBox.classList.toggle("active");
+  event.stopPropagation(); // Untuk menghentikan event klik dari menyebar ke luar elemen "text-box"
+});
+
+// Tambahkan event listener pada dokumen untuk menangani klik di luar "text-box"
+document.addEventListener("click", function (event) {
+  if (!textBox.contains(event.target) && !toggleButton.contains(event.target)) {
+    // Klik terjadi di luar elemen "text-box" dan tombol toggleButton
+    textBox.classList.remove("active");
+  }
 });
